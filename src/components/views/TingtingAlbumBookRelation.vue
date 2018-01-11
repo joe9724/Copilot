f9758993d3d6b35b2cf6a038051cbf5d692d8255
joe9724 <64060764@qq.com>
2018-01-11 23:08:09 +0800

    fsd

--- src/components/views/TingtingAlbumBookRelation.vue
+++ src/components/views/TingtingAlbumBookRelation.vue
@@ -2,24 +2,86 @@
   <section class="content">
 
     <div class="row center-block" style="background: #ffffff">
-      <!--
       <div id="example1_length" class="dataTables_length">
-        <router-link  class="pageLink" to="/album/add">
-          <a>
-            <span class="page" style="float:right;margin:5px"><el-button type="success" plain>添加专辑</el-button></span>
+        <div style="clear:both">
+          <div style="display:inline-block">
+            <el-input v-model="input" placeholder="请输入书名关键字"></el-input>
+          </div>
+          <div style="display:inline-block">
+            <el-button type="primary" icon="el-icon-search" @click="dialogTableVisible = true">从书库中搜索添加</el-button>
 
-          </a>
-        </router-link>
+          </div>
+        </div>
+        <el-dialog title="搜索" :visible.sync="dialogTableVisible">
+          <el-table :data="gridData">
+            <el-table-column property="date" label="日期" width="150"></el-table-column>
+            <el-table-column property="name" label="姓名" width="200"></el-table-column>
+            <el-table-column property="address" label="地址"></el-table-column>
+          </el-table>
+        </el-dialog>
+
 
       </div>
-      -->
-      <el-transfer
-        filterable
-        :filter-method="filterMethod"
-        filter-placeholder="请输入书名拼音"
-        v-model="value2"
-        :data="data2">
-      </el-transfer>
+      <table class="table table-bordered table-responsive table-striped">
+        <thead>
+        <tr>
+          <th style='text-align: center'>序号</th>
+          <th style='text-align: center'>书名</th>
+          <th style='text-align: center'>图标</th>
+          <th style='text-align: center'>作者</th>
+          <!--<th>副标题</th>-->
+          <!--<th>是否显示icon</th>-->
+          <!--<th>大图</th>-->
+          <th style='text-align: center'>章节数</th>
+          <th style='text-align: center'>播放数</th>
+          <!--<th>更新提示</th>-->
+          <!-- <th>播放地址</th>
+           <th>顺序</th>
+           <th>状态</th>
+           <th>时间</th>-->
+          <th style='text-align: center'>操作</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(item,index) in arrayData" v-bind:key="item.name">
+          <td style='text-align: center'>{{index+1}}</td>
+          <td style='text-align: center'>{{item.name}}</td>
+          <td style='text-align: center'><img v-bind:src=item.icon style="width: 60px;height:80px"> </td>
+          <td style='text-align: center'>{{item.authorName}}</td>
+          <!--<td class="sorting_1" style="vertical-align: middle">{{item.subTitle}}</td>-->
+          <!--<td class="sorting_1" style="vertical-align: middle">{{item.showIcon}}</td>-->
+          <!--<td class="sorting_1" style="vertical-align: middle">{{item.bigCover}}</td>-->
+          <td style='text-align: center'>{{item.clipsNumber}}</td>
+          <td style='text-align: center'>{{item.playCount}}</td>
+          <!--<td class="sorting_1" style="vertical-align: middle">{{item.duration}}</td>
+          &lt;!&ndash;<td class="sorting_1" style="vertical-align: middle">{{item.updateTips}}</td>&ndash;&gt;
+          <td class="sorting_1" style="vertical-align: middle">{{item.url}}</td>
+          <td class="sorting_1" style="vertical-align: middle">{{item.order}}</td>
+          <td class="sorting_1" style="vertical-align: middle">{{item.status | FormatStatus}}</td>
+          <td class="sorting_1" style="vertical-align: middle">{{item.time*1000 | BTKformatDate}}</td>-->
+          <td style='text-align: center'>
+            <el-button type="text">向上移动</el-button>
+            <el-button type="text">向下移动</el-button>
+            <el-button type="text" @click="editUser(item.id)" style="visibility: hidden">编辑</el-button>
+            <el-button type="text" @click="removeUser(item.id)">移除</el-button>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+      <div>
+        <div align="center">
+          <!--<span class="demonstration">调整每页显示条数</span>-->
+          <el-pagination
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :current-page.sync="currentPage"
+            :page-sizes="[10, 20, 30, 40]"
+            :page-size="10"
+            layout="prev, pager, next"
+            :total="totalCount">
+          </el-pagination>
+        </div>
+      </div>
       <!-- /.box-body -->
     </div>
 
@@ -28,40 +90,168 @@
 
 <script>
   // import $ from 'jquery'
-  // import api from '../../api'
-  // import { formatDateBtk, formatStatus } from '../../filters/index.js'
-
+  import api from '../../api'
+  import {formatDateBtk, formatStatus} from '../../filters/index.js'
   export default {
+    filters: {
+      BTKformatDate (time) {
+        var date = new Date(time)
+        return formatDateBtk(date, 'yyyy-MM-dd hh:mm')
+      },
+      FormatStatus (status) {
+        return formatStatus(status)
+      }
+    },
     data () {
-      const generateData2 = _ => {
-        const data = []
-        const cities = ['上海', '北京', '广州', '深圳', '南京', '西安', '成都']
-        const pinyin = ['shanghai', 'beijing', 'guangzhou', 'shenzhen', 'nanjing', 'xian', 'chengdu']
-        cities.forEach((city, index) => {
-          data.push({
-            label: city,
-            key: index,
-            pinyin: pinyin[index]
-          })
-        })
-        return data
-      }
       return {
-        data2: generateData2(),
-        value2: [],
-        filterMethod (query, item) {
-          // alert('aaa')
-          return item.pinyin.indexOf(query) > -1
-        }
+        currentPage: 1,
+        // 为第一页或者最后一页时，首页，尾页不能点击
+        fDisabled: false,
+        lDisabled: false,
+        // 总项目数
+        totalCount: 40,
+        // 分页数
+        pageCount: 20,
+        // 当前页面
+        pageCurrent: 1,
+        // 分页大小
+        pagesize: 10,
+        // 显示分页按钮数
+        showPages: 11,
+        // 开始显示的分页按钮
+        showPagesStart: 1,
+        // 结束显示的分页按钮
+        showPageEnd: 100,
+        // 分页数据
+        arrayData: [],
+        // 删除确认框
+        dialogVisible: false,
+        gridData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }],
+        dialogTableVisible: false
       }
-    }
+    },
+    methods: {
+      handleSizeChange (val) {
+        console.log(`每页 ${val} 条`)
+      },
+      removeUser (userId) {
+        this.$confirm('此操作将永久删除 ' + ', 是否继续?', '提示', {type: 'warning'})
+          .then(() => {
+            // 向请求服务端删除
+            var userid = localStorage.getItem('userid')
+            api.request('get', 'user/delete?user_id=' + userId + '&operator_id=' + userid)
+              .then(response => {
+                console.log(response.data)
+                this.$message.info('删除成功!')
+                // reload
+                api.request('get', 'user/list?operator_id=' + userid + '&page=1&size=10')
+                  .then(response => {
+                    console.log(response.data)
+                    this.arrayData = response.data.datas
+                  })
+                  .catch(error => {
+                    // this.$store.commit('TOGGLE_LOADING')
+                    console.log(error)
+                    this.response = error
+                  })
+              })
+              .catch(error => {
+                // this.$store.commit('TOGGLE_LOADING')
+                console.log(error)
+                this.response = error
+              })
+          })
+          .catch(() => {
+            this.$message.info('已取消操作!')
+          })
+      },
+      handleCurrentChange (val) {
+        console.log(`当前页: ${val}`)
+        var userid = localStorage.getItem('userid')
+        api.request('get', 'book/list?userid=' + userid + '&pageIndex=' + val + '&pageSize=10')
+          .then(response => {
+            console.log(response.data)
+            this.arrayData = response.data.body.bookList
+          })
+          .catch(error => {
+            // this.$store.commit('TOGGLE_LOADING')
+            console.log(error)
+            this.response = 'Server appears to be offline'
+          })
+      },
+      editUser (userId) {
+        // this.$router.push('/org/edit?orgid=' + agentId)
+        this.$router.push({path: '/user/edit?userid=' + userId})
+      }
+    },
+    created () {
+      // var userid = localStorage.getItem('userid')
+      api.request('get', 'book/list?userid=1&pageSize=12&pageIndex=1')
+        .then(response => {
+          console.log(response.data)
+          this.arrayData = response.data.body.bookList
+          for (var i = 0; i < this.arrayData.length; i++) {
+            this.arrayData.time = formatDateBtk(this.arrayData.time)
+            // this.arrayData.last_time = formatDateBtk(this.arrayData.last_time)
+            console.log()
+          }
+        })
+        .catch(error => {
+          // this.$store.commit('TOGGLE_LOADING')
+          console.log(error)
+          this.response = 'Server appears to be offline'
+        })
+    },
+    mounted () {
+      // this.showPage(this.pageCurrent, null, true)
+    },
+    computed: {}
   }
 
 </script>
 
 <style>
-  .transfer-footer {
-    margin-left: 20px;
-    padding: 6px 5px;
+  /* Using the bootstrap style, but overriding the font to not draw in
+     the Glyphicons Halflings font as an additional requirement for sorting icons.
+
+     An alternative to the solution active below is to use the jquery style
+     which uses images, but the color on the images does not match adminlte.
+
+  @import url('/static/js/plugins/datatables/jquery.dataTables.min.css');
+  */
+  @import url('/static/js/plugins/datatables/dataTables.bootstrap.css');
+
+  table.dataTable thead .sorting:after,
+  table.dataTable thead .sorting_asc:after,
+  table.dataTable thead .sorting_desc:after {
+    font-family: 'FontAwesome';
+  }
+
+  table.dataTable thead .sorting:after {
+    content: "\f0dc";
+  }
+
+  table.dataTable thead .sorting_asc:after {
+    content: "\f0dd";
+  }
+
+  table.dataTable thead .sorting_desc:after {
+    content: "\f0de";
   }
 </style>
