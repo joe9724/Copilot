fdb8b566d1a99d87593d444604d6384a5e024ec7
joe9724 <64060764@qq.com>
2018-01-23 02:18:39 +0800

    fsd

--- src/components/views/TingtingAddCategory.vue
+++ src/components/views/TingtingAddCategory.vue
@@ -31,10 +31,10 @@
             <el-form label="图片上传1">
 
             </el-form>
-            <quill-editor v-model="content"
-                          ref="myQuillEditor"
-                          :options="editorOption">
-            </quill-editor>
+            <vue-editor id="editor"
+                        useCustomImageHandler
+                        @imageAdded="handleImageAdded" v-model="htmlForEditor">
+            </vue-editor>
             <el-form-item>
               <el-button type="primary" @click="onSubmit">确定</el-button>
               <!--<el-button>确定</el-button>-->
@@ -47,13 +47,15 @@
 </template>
 <script>
   import api from '../../api'
-  import {quillRedefine} from 'vue-quill-editor-upload'
-  import {quillEditor} from 'vue-quill-editor'
+  import { VueEditor } from 'vue2-editor'
 
   export default {
-    components: {quillEditor, quillRedefine},
+    components: {
+      VueEditor
+    },
     data () {
       return {
+        htmlForEditor: '',
         editorOption: {},  // 必须初始化为对象 init  to Object
         canCrop: false,
         /* 测试上传图片的接口，返回结构为{url:''} */
@@ -146,50 +148,24 @@
             console.log(error)
             this.response = error
           })
+      },
+      handleImageAdded: function (file, Editor, cursorLocation) {
+        // An example of using FormData
+        // NOTE: Your key could be different such as:
+        // formData.append('file', file)
+
+        var formData = new FormData()
+        formData.append('file', file)
+        api.requestForm('post', 'file/upload', formData)
+          .then((response) => {
+            console.log('resultstr is', JSON.stringify(response.data.body))
+            let url = response.data.body.url // Get url from response
+            Editor.insertEmbed(cursorLocation, 'image', url)
+          })
+          .catch((err) => {
+            console.log(err)
+          })
       }
-    },
-    created () {
-      this.editorOption = quillRedefine(
-        {
-          // 图片上传的设置
-          uplpadConfig: {
-            action: 'http://127.0.0.1:81/nanjingyouzi/TingtingBackend/1.0.0/file/upload',  // 必填参数 图片上传地址
-            // 必选参数  res是一个函数，函数接收的response为上传成功时服务器返回的数据
-            // 你必须把返回的数据中所包含的图片地址 return 回去
-            res: (response) => {
-              return response.body.url
-            },
-            methods: 'POST',  // 可选参数 图片上传方式  默认为post
-            token: sessionStorage.token,  // 可选参数 如果需要token验证，假设你的token有存放在sessionStorage
-            name: 'file',  // 可选参数 文件的参数名 默认为img
-            size: 500,  // 可选参数   图片限制大小，单位为Kb, 1M = 1024Kb
-            accept: 'image/png, image/gif, image/jpeg, image/bmp, image/x-icon',  // 可选参数 可上传的图片格式
-            // input点击事件  formData是提交的表单实体
-            change: (formData) => {
-            },
-            // 设置请求头 xhr: 异步请求， formData: 表单对象
-            header: (xhr, formData) => {
-              // xhr.setRequestHeader('myHeader','myValue');
-              // formData.append('token', '1234')
-            },
-            // start: function (){}
-            start: () => {
-            },  // 可选参数 接收一个函数 开始上传数据时会触发
-            end: () => {
-            },  // 可选参数 接收一个函数 上传数据完成（成功或者失败）时会触发
-            success: () => {
-            },  // 可选参数 接收一个函数 上传数据成功时会触发
-            error: () => {
-            }  // 可选参数 接收一个函数 上传数据中断时会触发
-          },
-          // 以下所有设置都和vue-quill-editor本身所对应
-          placeholder: '请xxx',  // 可选参数 富文本框内的提示语
-          theme: 'snow',  // 可选参数 富文本编辑器的风格
-          toolOptions: [],  // 可选参数  选择工具栏的需要哪些功能  默认是全部
-          handlers: {}  // 可选参数 重定义的事件，比如link等事件
-        }
-      )
-      console.log(this.editorOption)
     }
   }
 </script>
