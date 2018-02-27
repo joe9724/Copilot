866aeeb39db3616f5895817f13f21db6ee21fbc6
joe9724 <64060764@qq.com>
2018-02-27 20:54:50 +0800

    s

--- src/config/index.js
+++ src/config/index.js
@@ -2,14 +2,14 @@ export default {
   // serverURI: 'http://tingting-backend-api.bitekun.xin/nanjingyouzi/TingtingBackend/1.0.0/',
   // serverURI: 'http://192.168.200.208:81/nanjingyouzi/TingtingBackend/1.0.0/',
   // a
-  // serverURI: 'http://127.0.0.1:81/nanjingyouzi/TingtingBackend/1.0.0/',
-  // uploadURI: 'http://127.0.0.1:81/nanjingyouzi/TingtingBackend/1.0.0/file/upload',
+  serverURI: 'http://127.0.0.1:81/nanjingyouzi/TingtingBackend/1.0.0/',
+  uploadURI: 'http://127.0.0.1:81/nanjingyouzi/TingtingBackend/1.0.0/file/upload',
 
   // serverURI: 'http://192.168.200.208:81/nanjingyouzi/TingtingBackend/1.0.0/',
   // uploadURI: 'http://192.168.200.208:81/nanjingyouzi/TingtingBackend/1.0.0/file/upload',
 
-  serverURI: 'http://tingting-backend-api.bitekun.xin/nanjingyouzi/TingtingBackend/1.0.0/',
-  uploadURI: 'http://tingting-backend-api.bitekun.xin/nanjingyouzi/TingtingBackend/1.0.0/file/upload',
+  // serverURI: 'http://tingting-backend-api.bitekun.xin/nanjingyouzi/TingtingBackend/1.0.0/',
+  // uploadURI: 'http://tingting-backend-api.bitekun.xin/nanjingyouzi/TingtingBackend/1.0.0/file/upload',
   // a
   // serverURI: 'http://collector.bitekun.xin/',
   fixedLayout: false,
